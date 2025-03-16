79d4ec59f1bc131d8a7843c704532cf615837fef
edselspth <edselspth@gmail.com>
2025-03-16 08:06:06 +0700

    menambahkan Class DataGeneric

---
+++ tpModul5_103022300016/DataGeneric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul5_103022300016
+{
+    class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void printData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah : " + data);
+        }
+    }
+}
--- tpModul5_103022300016/Program.cs
+++ tpModul5_103022300016/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpModul5_103022300016;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        DataGeneric<String> dataNIM = new DataGeneric<String>("103022300016");
+        dataNIM.printData();
+        DataGeneric<int> dataUmur = new DataGeneric<int>(20);
+        dataUmur.printData();
+    }
+}
