5ac16c43303028fdde444a8c3befa10b69649eae
edselspth <edselspth@gmail.com>
2025-03-16 07:31:32 +0700

    Menambahkan Class HaloGeneric dengan Method SapaUser

---
+++ tpModul5_103022300016/HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul5_103022300016
+{
+    class HaloGeneric
+    {
+        public void halo<T>(T nama)
+        {
+            Console.WriteLine("Halo User " + nama);
+        }
+    }
+}
--- tpModul5_103022300016/Program.cs
+++ tpModul5_103022300016/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpModul5_103022300016;
+public class Program
+{
+    public static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        String nama = "Edsel Septa Haryanto"; // tipe data string
+        halo.halo(nama);
+        int angkaInt = 2; // tipe data int
+        halo.halo(angkaInt);
+        double angkaDouble = 2.5; // tipe data double
+        halo.halo(angkaDouble);
+    }
+}
